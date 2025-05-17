a9d55c42d4318158577cc47e0913f30b53f893a5
darel-developer <sanangdarel17@gmail.com>
2025-05-17 16:26:32 +0100

    création de la tables annonces

--- database/migrations/2025_05_17_152532_create_annonces_table.php
+++ database/migrations/2025_05_17_152532_create_annonces_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-       
-         Schema::create('announcements', function (Blueprint $table) {
+        Schema::create('annonces', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('message');
+            $table->text('classes')->nullable();
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('announcements');
+        Schema::dropIfExists('annonces');
     }
 };
