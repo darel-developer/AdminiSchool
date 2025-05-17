ccc1fb7e4fd42820eb254e2c87f980914bd6c5c1
darel-developer <sanangdarel17@gmail.com>
2025-05-17 15:28:12 +0100

    correction annonce

--- resources/views/eventschool.blade.php
+++ resources/views/eventschool.blade.php
@@ -669,6 +669,7 @@
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             const alert = document.querySelector('.alert');
